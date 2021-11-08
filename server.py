58c209376c7a1214b06fee8d7bce8cfffc408b60
Lilo Heinrich <liloanncat@gmail.com>
2021-11-08 14:12:49 -0500

    upload files

--- server.py
+++ server.py
@@ -7,15 +7,45 @@
 
 import socket
 
-s = socket.socket()
-host = socket.gethostname()
-# host = '' #ip of raspberry pi
-port = 12345
-s.bind((host, port))
+# HOST = '' # Server IP or Hostname
+# HOST = socket.gethostname()
+# HOST = '127.0.1.1'
+HOST = '10.27.91.11'
+print("HOST = ", HOST)
+PORT = 12345 # Pick an open Port (1000+ recommended), must match the client sport
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Socket created')
+
+#managing error exception
+try:
+  s.bind((HOST, PORT))
+except socket.error:
+  print('Bind failed')
 
 s.listen(5)
+print('Socket awaiting messages')
+(conn, addr) = s.accept()
+print('Connected')
+
+# awaiting for message
 while True:
-  c, addr = s.accept()
-  print ('Got connection from',addr)
-  c.send('Thank you for connecting')
-  c.close()
+  data = conn.recv(1024)
+  print('I sent a message back in response to: ' + data) 
+  reply = ''
+
+  # process your message
+  if data == 'Hello':
+    reply = 'Hi, back!'
+  elif (data == 'This is important'):
+    reply = 'OK, I have done the important thing you have asked me!'
+  #and so on and on until...
+  elif data == 'quit':
+    conn.send('Terminating')
+    break
+  else:
+    reply = 'Unknown command'
+
+  # Sending reply
+  conn.send(reply)
+  
+conn.close() # Close connections
