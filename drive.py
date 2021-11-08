d3799e70ed1e8364f002c3ff7632c0c03fe8ecdd
Lilo Heinrich <liloanncat@gmail.com>
2021-11-07 20:38:22 -0500

    add integrated joystick drive! next: map to controllers

---
+++ drive.py
@@ -0,0 +1,101 @@
+import numpy as np
+import math
+
+x_rot = np.divide([1,1,-1,-1], math.sqrt(2.0))
+y_rot = np.divide([1,-1,-1,1], math.sqrt(2.0))
+
+def getsign(m):
+    if m >= 0.0:
+        return 1
+    return -1
+
+def clamp(n, minn, maxn):
+    return max(min(maxn, n), minn)
+
+# drive() takes params describing desired driving movement, 
+# outputs the direction, angle, and speed to set each module.
+# 
+# inputs:
+#   s_dir = strafe direction (angle from 0 to 360),
+#   s_mag = strafe magnitude (0.0 to 1.0 speed scalar),
+#   r_dir = rotate direction (true = cw, false = ccw),
+#   r_mag = rotate magnitude (0.0 to 1.0 speed scalar)
+# 
+# returns: 
+#   motor fwd/rev (1/-1),
+#   motor speed (0.0 to 1.0), 
+#   servo angle (0.0 to 180.0 degrees) 
+def drive_vector(s_dir, s_mag, r_dir, r_mag):
+    # start by calculating x and y scaled vector components
+    x_s = math.sin(s_dir) * s_mag / math.sqrt(2.0)
+    y_s = math.cos(s_dir) * s_mag / math.sqrt(2.0)
+    x_r = x_rot * r_mag
+    y_r = y_rot * r_mag
+    if not r_dir: # flip the rotation vectors if ccw
+        x_r *= -1
+        y_r *= -1
+    
+    # xy vector addition on the rotate and stafe vectors
+    res_x = [x_s + n for n in x_r]
+    res_y = [y_s + n for n in y_r]
+
+    # convert xy to magnitude and angle
+    res_mag = [math.sqrt(res_x[i]**2 + res_y[i]**2) for i in range(4)]
+    res_ang = [math.atan(res_y[i] / res_x[i]) for i in range(4)] # todo: fix x = 0 error
+
+    # separate motor magnitude (speed) and direction (fwd/rev)
+    res_dir = [getsign(m) for m in res_mag]
+    res_mag = [abs(m) for m in res_mag]
+
+    # scale magnitudes back into 0.0 - 1.0 range in case (1.0 should be max speed)
+    if any([r > 1.0 for r in res_mag]):
+        max_mag = max(res_mag)
+        res_mag = [(m / max_mag) for m in res_mag]
+
+    # scale angles back into 180 degree servo range - reverse motors if necessary
+    if any([a > 180.0 for a in res_ang]) > 180.0:
+        res_ang = [a - 180.0 for a in res_ang]
+        res_dir = [-d for d in res_dir]
+
+    # gives motor fwd/rev (1/-1), motor speed (0.0 to 1.0), servo angle (0.0 to 180.0 degrees)
+    return res_dir, res_mag, res_ang
+
+
+# like drive_vector, expect it takes x_s x-strafe component from
+# from -1.0 to 1.0, and y_s y-strafe component from -1.0 to 1.0, 
+# and rotate as a signed value rather than r_dir and r_mag.
+# meant to interface with joystick axes easily.
+def drive(x_s, y_s, r_s):
+    # start by calculating x and y scaled vector components
+    r_dir = getsign(r_s)
+    r_mag = abs(r_s)
+    x_r = x_rot * r_mag
+    y_r = y_rot * r_mag
+    if not r_dir: # flip the rotation vectors if ccw
+        x_r *= -1
+        y_r *= -1
+
+    # xy vector addition on the rotate and stafe vectors
+    res_x = [x_s + n for n in x_r]
+    res_y = [y_s + n for n in y_r]
+
+    # convert xy to magnitude and angle
+    res_mag = [math.sqrt(res_x[i]**2 + res_y[i]**2) for i in range(4)]
+    res_ang = [math.atan(res_y[i] / res_x[i]) for i in range(4)] # todo: fix x = 0 error
+
+    # separate motor magnitude (speed) and direction (fwd/rev)
+    res_dir = [getsign(m) for m in res_mag]
+    res_mag = [abs(m) for m in res_mag]
+
+    # scale magnitudes back into 0.0 - 1.0 range in case (1.0 should be max speed)
+    if any([r > 1.0 for r in res_mag]):
+        max_mag = max(res_mag)
+        res_mag = [(m / max_mag) for m in res_mag]
+
+    # scale angles back into 180 degree servo range - reverse motors if necessary
+    if any([a > 180.0 for a in res_ang]) > 180.0:
+        res_ang = [a - 180.0 for a in res_ang]
+        res_dir = [-d for d in res_dir]
+
+    # gives motor fwd/rev (1/-1), motor speed (0.0 to 1.0), servo angle (0.0 to 180.0 degrees)
+    return res_dir, res_mag, res_ang
---
+++ joydrive.py
@@ -0,0 +1,80 @@
+from drive import drive, clamp
+import joystickapi
+import time
+import math
+
+# 0: select 
+# 1: left joystick press
+# 2: right joystick press
+# 3: start
+# 4: dpad up
+# 5: dpad right
+# 6: dpad down
+# 7: dpad left
+# 8: right joystick press
+# 9: left trigger
+# 10: right trigger
+# 11: left bumper
+# 12: right bumper
+# 13: green triangle up
+# 14: red circle right
+# 15: blue x down
+# 16: purple square left
+# 17: middle button clear
+
+# axis 0/X: left joystick R-L axis
+# axis 1/Y: left joystick up-down axis
+# axis 2/Z: right joystick R-L axis
+# axis 3/R: right joystick up-down axis
+
+
+# square to circle joystick correction
+# https://www.xarg.org/2017/07/how-to-map-a-square-to-a-circle/
+def map(x, y):
+    return x * math.sqrt(1 - y*y/2.0), y * math.sqrt(1 - x*x/2.0)
+
+
+print("start")
+deadband = 500
+# default_joyvalue = -256
+max_joyvalue = 32768
+rate = 0.1 # sleep between loops in seconds
+
+num = joystickapi.joyGetNumDevs()
+ret, caps, startinfo = False, None, None
+for id in range(num):
+    ret, caps = joystickapi.joyGetDevCaps(id)
+    if ret:
+        print("gamepad detected: " + caps.szPname)
+        ret, startinfo = joystickapi.joyGetPosEx(id)
+        break
+else:
+    print("no gamepad detected")
+
+run = ret
+while run:
+    time.sleep(rate)
+    ret, info = joystickapi.joyGetPosEx(id)
+    if ret:
+        btns = [(1 << i) & info.dwButtons != 0 for i in range(caps.wNumButtons)][0:17]
+        axisXYZR = [info.dwXpos-startinfo.dwXpos, info.dwYpos-startinfo.dwYpos, info.dwZpos-startinfo.dwZpos, info.dwRpos-startinfo.dwRpos]
+        if info.dwButtons:
+            print("buttons: ", btns)
+        if any([abs(v) > deadband for v in axisXYZR[0:3]]):
+            # print("axis:", axisXYZR)
+
+            x_s_raw = clamp(axisXYZR[0] / max_joyvalue, -1.0, 1.0)
+            y_s_raw = clamp(axisXYZR[1] / max_joyvalue, -1.0, 1.0)
+            r_s = clamp(axisXYZR[2] / max_joyvalue, -1.0, 1.0)
+            x_s, y_s = map(x_s_raw, y_s_raw)
+            # print("x_s, y_s, r_s", round(x_s, 4), round(y_s, 4), round(r_s, 4))
+
+            res_dir, res_mag, res_ang = drive(x_s, y_s, r_s)
+            res_mag = [round(r, 3) for r in res_mag]
+            res_ang = [round(math.degrees(a), 1) for a in res_ang]
+            print("res_dir, res_mag, res_ang", res_dir, res_mag, res_ang)
+
+        else:
+            print("inactive, under deadband")
+
+print("end")
